b0f2c4bb81864fcd000f78275204e791bf89fc39
StephenFisayo <102196039+StephenFisayo@users.noreply.github.com>
2024-05-14 10:58:07 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { React } from 'react'
+import PictureBoard from './PictureBoard'
+import Navbar from './Navbar'
+import Modal from './Modal'
+
+
+function App(){
+   return(
+     <div>
+       <Navbar />
+      <PictureBoard />
+       <Modal />
+     </div>
+   )
+
+}
+
+export default App
